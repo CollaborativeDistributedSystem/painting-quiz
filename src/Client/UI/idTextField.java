6c71aade95ce760ea5d0f0a2892d3d782a9338aa
HunkiKim <gnsrl76@naver.com>
2021-05-21 10:47:14 +0900

    TextField and Brush add

---
+++ src/Client/UI/idTextField.java
@@ -0,0 +1,10 @@
+package Client.UI;
+
+import javax.swing.JTextField;
+
+public class idTextField extends JTextField{
+    public idTextField() {
+        setBounds(50,30,120,30);
+        setHorizontalAlignment(JTextField.CENTER);
+    }
+}
