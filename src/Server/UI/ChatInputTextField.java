7cf092ea2b73a7dc1b9f4091c8eed272362c111e
HunkiKim <gnsrl76@naver.com>
2021-05-24 13:45:22 +0900

    Server UI Update

---
+++ src/Server/UI/ChatInputTextField.java
@@ -0,0 +1,5 @@
+package Server.UI;
+
+
+public class ChatInputTextField {
+}
---
+++ src/Server/UI/Startbtn.java
@@ -0,0 +1,9 @@
+package Server.UI;
+import javax.swing.JButton;
+
+public class Startbtn extends JButton{
+    public Startbtn(){
+        super("Start");
+        setBounds(10,10,500,50);
+    }
+}
